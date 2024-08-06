b53eea842d0caba03fce1e81e44b4a33f0e72598
saadtayel <www.saadtayel6@gmail.com>
2024-08-06 19:51:01 +0300

    Initial commit

--- js/main.js
+++ js/main.js
@@ -6,3 +6,12 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const logoNav = document.querySelector('.menu');
+    const ulNav = document.querySelector('.ul-list ul');
+
+    logoNav.addEventListener('click', () => {
+        ulNav.classList.toggle('active');
+    });
+});
